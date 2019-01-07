5d5a90dd10ea6faf18e0f03c045bbac031991d9c
Chloe <chloemontgomery01@gmail.com>
2019-01-07 15:12:14 +0000

    Add sort_value method

--- second_challenge.rb
+++ second_challenge.rb
@@ -3,6 +3,18 @@ def add_one(hash)
   print hash
 end
 
+def sort_value(hash)
+  keys = []
+  values = []
+  hash.each do |k, v|
+    keys << k
+    values << v
+  end
+  keys.sort!
+  values.sort!
+  print keys.zip(values).to_h
+end
+
 def sorted_values(hash)
   my_hash = {}
   my_hash = hash.values.sort
