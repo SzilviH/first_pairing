ff6414f203c02a4b446a6b956081a3f7b1f65727
Chloe <chloemontgomery01@gmail.com>
2019-01-07 14:06:52 +0000

    Add methods

--- first_challenge.rb
+++ first_challenge.rb
@@ -1,5 +1,28 @@
 def add_one(arr)
+  my_array = []
   arr.each do |n|
-    n += 1
+    my_array << n += 1
   end
+  print my_array
+end
+
+def sort_it(arr)
+  my_array = arr.sort
+  print my_array
+end
+
+def sort_add_one(arr)
+  my_array = []
+  arr.sort.each do |n|
+    my_array << n += 1
+  end
+  print my_array
+end
+
+def sum_it(arr)
+  arr.sum
+end
+
+def twice_the_sum(arr)
+  arr.sum * 2
 end
